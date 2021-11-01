a1d893b047ebfdea9a273d991cc4c6eb07e2e5c0
ikky <ikramikky4462@gmail.com>
2021-11-01 14:41:53 +0300

    Created BaseConvRNN module, inherited by ConvLSTM and ConvGRU

--- conv_rnns.py
+++ conv_rnns.py
@@ -3,27 +3,6 @@ import torch
 import torch.nn as nn
 from torch.functional import Tensor
 
-class ConvRNN(nn.Module):
-    def __init__(self, x_in:int, intermediate_channels:int, kernel_size:int=3):
-        super().__init__()
-        self.kernel_size = kernel_size
-        self.conv_x = nn.Conv2d(
-            x_in + intermediate_channels, intermediate_channels,
-            kernel_size=kernel_size, padding= kernel_size // 2, bias=True
-        )
-        self.intermediate_channels = intermediate_channels
-
-    def forward(self, x:Tensor, state:Tensor) -> Tensor:
-        x = torch.cat([x, state], dim=1)
-        x = self.conv_x(x)
-        return torch.tanh(x)
-
-    def init_states(self, batch_size:int, channels:int, spatial_dim:typing.Tuple) -> Tensor:
-        width, height = spatial_dim
-        y = torch.zeros(batch_size, channels, width, height, device=self.conv_x.weight.device)
-        return y
-
-
 class ConvLSTMCell(nn.Module):
     def __init__(self, x_in:int, intermediate_channels:int, kernel_size:int=3):
         super(ConvLSTMCell, self).__init__()
@@ -52,7 +31,7 @@ class ConvLSTMCell(nn.Module):
         h = b * torch.tanh(c)
         return c, h
 
-    def init_states(self, batch_size:int, channels:int, spatial_dim:typing.Tuple) -> typing.Tuple:
+    def _init_states(self, batch_size:int, channels:int, spatial_dim:typing.Tuple) -> typing.Tuple:
         width, height = spatial_dim
         c = torch.zeros(batch_size, channels, width, height, device=self.conv_x.weight.device)
         h = torch.zeros(batch_size, channels, width, height, device=self.conv_x.weight.device)
@@ -87,25 +66,25 @@ class ConvGRUCell(nn.Module):
         h = a * h + (1 - a) * y
         return h
 
-    def _init_state(self, batch_size:int, channels:int, spatial_dim:typing.Tuple) -> Tensor:
+    def _init_states(self, batch_size:int, channels:int, spatial_dim:typing.Tuple) -> Tensor:
         width, height = spatial_dim
         h = torch.zeros(batch_size, channels, width, height, device=self.conv_x.weight.device)
         return h
 
-class ConvLSTM(nn.Module):
-    def __init__(self, num_cells:int, in_channels:int, intermediate_channels:int, kernel_size:int, return_sequence:bool=False, return_cell_states:bool=False):
-        super().__init__()
+class BaseConvRNN(nn.Module):
+    def __init__(self, cell, num_cells:int, in_channels:int, intermediate_channels:int, kernel_size:int, return_sequence=True, return_cell_states=True):
+        super(BaseConvRNN, self).__init__()
         self.num_cells = num_cells
         self.kernel_size = kernel_size
         self.intermediate_channels = intermediate_channels
         self.return_sequence = return_sequence
         self.return_cell_states = return_cell_states
-        lstm_cells = []
+        cells = []
         for _ in range(num_cells):
-            lstm_cells.append(ConvLSTMCell(in_channels, intermediate_channels, kernel_size))
-        self.lstm_cells = nn.ModuleList(lstm_cells)
+            cells.append(cell(in_channels, intermediate_channels, kernel_size))
+        self.cells = nn.ModuleList(cells)
 
-    def forward(self, x:Tensor, state:typing.Tuple=None) -> typing.Tuple:
+    def _get_states(self, x:Tensor, state:typing.Tuple=None) -> typing.List:
         b, s, _, w, h = x.shape
         cell_states = []
 
@@ -113,70 +92,51 @@ class ConvLSTM(nn.Module):
             seq_states = []
             for i in range(s):
                 if i == 0 and n == 0: #first cell, first sequence
-                    prev_state = self.lstm_cells[n].init_states(b, self.intermediate_channels, (w, h))
+                    prev_state = self.cells[n]._init_states(b, self.intermediate_channels, (w, h))
                 elif  n > 0 and i > 0: #not first cell but sequence > 0
                     prev_state = seq_states[n - 1]
                 elif n > 0 and i == 0: #not first cell but starting first sequence
                     prev_state = cell_states[i - 1][-1]
                 else:
                     NotImplementedError()
-                current_state = self.lstm_cells[n](x[:, i, :, :, :], prev_state)
+                current_state = self.cells[n](x[:, i, :, :, :], prev_state)
                 seq_states.append(current_state)
             cell_states.append(seq_states)
 
-        cell_seq_states = cell_states[:][-1]
-        last_cell_state = cell_seq_states[-1]
-
-        if self.return_cell_states and self.return_sequence:
-            return cell_seq_states, cell_states
-        elif not self.return_sequence and not self.return_cell_states:
-            return last_cell_state, None
-
-        return last_cell_state , cell_states
+        return cell_states
 
 
-class ConvGRU(nn.Module):
+class ConvLSTM(BaseConvRNN):
     def __init__(self, num_cells:int, in_channels:int, intermediate_channels:int, kernel_size:int, return_sequence:bool=False, return_cell_states:bool=False):
-        super().__init__()
-        self.num_cells = num_cells
-        self.kernel_size = kernel_size
-        self.intermediate_channels = intermediate_channels
-        self.return_sequence = return_sequence
-        self.return_cell_states = return_cell_states
-        gru_cells = []
-        for _ in range(num_cells):
-            gru_cells.append(ConvLSTMCell(in_channels, intermediate_channels, kernel_size))
-        self.gru_cells = nn.ModuleList(gru_cells)
-
+        super(ConvLSTM, self).__init__(
+            ConvLSTMCell, num_cells, in_channels,
+            intermediate_channels, kernel_size, return_sequence,
+            return_sequence
+        )
     def forward(self, x:Tensor, state:typing.Tuple=None) -> typing.Tuple:
-        b, s, _, w, h = x.shape
-        cell_states = []
-
-        for n in range(self.num_cells):
-            seq_states = []
-            for i in range(s):
-                if i == 0 and n == 0: #first cell, first sequence
-                    prev_state = self.gru_cells[n].init_states(b, self.intermediate_channels, (w, h))
-                elif  n > 0 and i > 0: #not first cell but sequence > 0
-                    prev_state = seq_states[n - 1]
-                elif n > 0 and i == 0: #not first cell but starting first sequence
-                    prev_state = cell_states[i - 1][-1]
-                else:
-                    NotImplementedError()
-                current_state = self.gru_cells[n](x[:, i, :, :, :], prev_state)
-                seq_states.append(current_state)
-            cell_states.append(seq_states)
-
-        cell_seq_states = cell_states[:][-1]
-        last_cell_state = cell_seq_states[-1]
-
+        states = self._get_states(x)
         if self.return_cell_states and self.return_sequence:
-            return cell_seq_states, cell_states
+            return states[:][-1], states
         elif not self.return_sequence and not self.return_cell_states:
-            return last_cell_state, None
+            return states[-1][-1], None
 
-        return last_cell_state , cell_states
+        return states[-1][-1] , states
 
+class ConvGRU(BaseConvRNN):
+    def __init__(self, num_cells:int, in_channels:int, intermediate_channels:int, kernel_size:int, return_sequence:bool=False, return_cell_states:bool=False):
+        super(ConvGRU, self).__init__(
+            ConvGRUCell, num_cells, in_channels,
+            intermediate_channels, kernel_size, return_sequence,
+            return_sequence
+        )
+    def forward(self, x:Tensor, state:typing.Tuple=None) -> typing.Tuple:
+        states = self._get_states(x)
+        if self.return_cell_states and self.return_sequence:
+            return states[:][-1], states
+        elif not self.return_sequence and not self.return_cell_states:
+            return states[-1][-1], None
+
+        return states[-1][-1] , states
 
 
 
